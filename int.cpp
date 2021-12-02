1ff8eb548f21af2ccb81c09ee2976de21b0f69aa
Michael07220823 <michael31703@gmail.com>
2021-12-02 16:00:25 +0800

    Updated.

--- int.cpp
+++ int.cpp
@@ -8,14 +8,17 @@ int main(int argc, char const *argv[])
     int a = 0;
     int b = 0;
     int c = 0;
+    int d = 0;
     
     a = (a + 1) * 100;
     b = (b - 10000) / 10;
     c = a * b;
+    d = c % a;
 
     cout << "a: " << a << endl;
     cout << "b: " << b << endl;
     cout << "c: " << c << endl;
+    cout << "d: " << d << endl;
 
     return 0;
 }
